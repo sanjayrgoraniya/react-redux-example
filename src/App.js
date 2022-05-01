727cdc80f6d7ea6d08d8d5f0025446be2822910a
Sanjay Goraniya <sanjayrgoraniya@gmail.com>
2022-05-01 23:14:06 +0530

    feat: add react boilerplate

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
+          Learn React
+        </a>
+      </header>
+      <footer className="relative pt-8 pb-6 mt-16">
+        <div className="container mx-auto px-4">
+            <div className="flex flex-wrap items-center md:justify-between justify-center">
+                <div className="w-full md:w-6/12 px-4 mx-auto text-center">
+                    <div className="text-sm text-blueGray-500 font-semibold py-1">
+                        Made with Love by <a href="https://sanjaygoraniya.github.io" className="hover:text-blueGray-500 text-blueGray-800" target="_blank" rel="noopener noreferrer">Sanjay Goraniya</a>.
+                    </div>
+                </div>
+            </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
